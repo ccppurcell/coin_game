749f0723d705dc568ab277b0ef31234d9929ecef
ccppurcell <ccppurcell@gmail.com>
2025-11-14 16:32:32 +0100

    removes sanity check test; adds test_player_cannot_move_out_of_bounds

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,39 @@
 import pygame
 
-def test_sanity():
-    assert 1+1 == 2
+from coin_game.main import (
+        Object, Robot, Monster, border_w, border_h, height, width)
 
+def test_player_cannot_move_out_of_bounds():
+
+    #init the player
+    player = Robot()
+
+    #first target is top right corner
+    target1_x, target1_y = border_w, border_h
+
+    #move player to target
+    player.x, player.y = target1_x, target1_y
+
+    #attempt to move player outside the border
+    player.move_left()
+    player.move_up()
+
+    #player shouldn't move
+    assert player.x == target1_x
+    assert player.y == target1_y
+
+    #second target is bottom left corner
+    target2_x = width - border_w - player.width
+    target2_y = height - border_h - player.height
+
+    #move player to target
+    player.x, player.y = target2_x, target2_y
+
+    #attempt to move player outside the border
+    player.move_right()
+    player.move_down()
+    
+    #player shouldn't move
+    assert player.x == target2_x
+    assert player.y == target2_y
 
